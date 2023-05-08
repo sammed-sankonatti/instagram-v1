3e5832816184bc2578f4d4f89bfef8c25059b3c0
sammed-sankonatti <sammed.sankonatti18@gmail.com>
2023-05-08 17:15:07 +0530

    first version of instagram

---
+++ src/components/assets/dummyData.js
@@ -0,0 +1,59 @@
+export const posts = [
+    {
+        _id : "123",
+        postedBy : {
+            name : "sam",
+            _id : "123"
+        },
+        photo : "https://i.pinimg.com/564x/81/1b/52/811b52ea5e6f779359f404ee815419f9.jpg",
+        likes : [],
+        title : "insta v1",
+        caption : "first version on instagram...",
+        comments : [
+            {
+                postedBy : {
+                    name : "manoj"
+                },
+                text : "good creation"
+            }
+        ]
+    },
+    {
+        _id : "123",
+        postedBy : {
+            name : "virat",
+            _id : "123"
+        },
+        photo : "https://wallpapercave.com/wp/wp6426535.jpg",
+        likes : [],
+        title : "insta v1",
+        caption : "first version on instagram...",
+        comments : [
+            {
+                postedBy : {
+                    name : "kavi"
+                },
+                text : "good creation"
+            }
+        ]
+    },
+    {
+        _id : "123",
+        postedBy : {
+            name : "RCB",
+            _id : "123"
+        },
+        photo : "https://twistagram18.netlify.app/assets/king_ab_max-03ef8b7e.jpg",
+        likes : [],
+        title : "insta v1",
+        caption : "first version on instagram...",
+        comments : [
+            {
+                postedBy : {
+                    name : "deeksha"
+                },
+                text : "good creation"
+            }
+        ]
+    }
+]
